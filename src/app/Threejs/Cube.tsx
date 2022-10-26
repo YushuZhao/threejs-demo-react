26fad38485e27dc70496b1ace333f8f21555b8d1
yushu zhao <yushu5352@163.com>
2022-10-26 15:36:12 +0800

    fix: delete useless dependencies

--- src/app/Threejs/Cube.tsx
+++ src/app/Threejs/Cube.tsx
@@ -1,4 +1,3 @@
-import { render } from "@testing-library/react";
 import React, { useEffect } from "react";
 import * as THREE from "three";
 import Stats from "three/examples/jsm/libs/stats.module";
